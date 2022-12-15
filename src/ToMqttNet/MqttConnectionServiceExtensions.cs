5753bd7f4f23485571111e7685286b22abac5265
dave <dave@torutek.com>
2022-12-15 13:39:32 +1300

    Helper methods for making topics
    Matches what the HA documentation uses

--- src/ToMqttNet/MqttConnectionServiceExtensions.cs
+++ src/ToMqttNet/MqttConnectionServiceExtensions.cs
@@ -15,22 +15,54 @@ namespace ToMqttNet
 			Formatting = Formatting.None,
 			NullValueHandling = NullValueHandling.Ignore,
 		};
+
 		public static string ToJson<T>(this T config) where T : MqttDiscoveryConfig
 		{
 			return JsonConvert.SerializeObject(config, DiscoveryJsonSettings);
+		}
+
+		/// <summary>
+		/// Populate the <see cref="IMqttDiscoveryDeviceWithState.StateTopic"/> of the given config object, and return it
+		/// </summary>
+		public static T PopulateStateTopic<T>(this T config, IMqttConnectionService connect) where T : MqttDiscoveryConfig, IMqttDiscoveryDeviceWithState
+		{
+			config.StateTopic = connect.GetStateTopic(config);
+			return config;
 		}
 	}
 
 	public static class MqttConnectionServiceExtensions
 	{
+		/// <summary>
+		/// Publish the given <see cref="MqttDiscoveryConfig"/> so Home Assistant will discover it
+		/// </summary>
 		public static Task PublishDiscoveryDocument<T>(this IMqttConnectionService connection, T config) where T : MqttDiscoveryConfig
 		{
 			return connection.PublishAsync(
 				new MqttApplicationMessageBuilder()
-					.WithTopic($"homeassistant/{config.Component}/{connection.MqttOptions.NodeId}/{config.UniqueId}/config")
+					.WithTopic(connection.GetTopic(config, "config"))
 					.WithRetainFlag()
 					.WithPayload(config.ToJson())
 					.Build());
+		}
+
+		/// <summary>
+		/// Get the tpoic that the given <see cref="MqttDiscoveryConfig"/> should use with the given <paramref name="leaf"/>
+		/// </summary>
+		/// <example>
+		/// homeassistant/binary_sensor/my-node/my-sensor/state
+		/// </example>
+		public static string GetTopic(this IMqttConnectionService connection, MqttDiscoveryConfig config, string leaf)
+		{
+			return $"homeassistant/{config.Component}/{connection.MqttOptions.NodeId}/{config.UniqueId}/{leaf}";
+		}
+
+		/// <summary>
+		/// Get the state topic that the given <see cref="MqttDiscoveryConfig"/> should use.
+		/// </summary>
+		public static string GetStateTopic(this IMqttConnectionService connection, MqttDiscoveryConfig config)
+		{
+			return connection.GetTopic(config, "state");
 		}
 	}
 }
